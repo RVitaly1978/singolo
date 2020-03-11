694d91efc0ac4b69839b2407b91327592d69d9f4
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-03-11 12:04:48 +0300

    feat: add add/remove active-class to links in navbar

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('script loaded');
+
+const NAVBAR = document.querySelector('#navbar');
+
+function navbarHandler(event) {
+  if (event.target.tagName !== 'A') return;
+
+  NAVBAR.querySelectorAll('li > a').forEach(elem => {
+    elem.classList.remove('navbar__link--active');
+  });
+
+  event.target.classList.add('navbar__link--active');
+}
+
+NAVBAR.addEventListener('click', navbarHandler);
